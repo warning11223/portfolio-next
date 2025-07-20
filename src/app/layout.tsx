46a74fefe99b72866b1b7a1735da882ea766c766
warning11223 <warning11223@gmail.com>
2025-07-20 21:06:42 +0300

    fix: seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,39 @@ const calistoga = Calistoga({
     weight: ["400"]
 })
 
+const siteUrl = "https://denis-frontend.ru/"
+
 export const metadata: Metadata = {
-    title: "Frontend developer portfolio",
-    description: "I'm a frontend developer and I specialize in creating and optimizing high-load web applications",
+    title: "Frontend Web Development",
+    description: "Discover projects and insights from a frontend developer passionate about building modern, fast, and accessible web apps.",
+    keywords: ["Frontend developer", "Next.js", "React", "Web development", "UI/UX", "High-load applications", "TypeScript", "JavaScript", "Redux", "Vue", "Nuxt", "HTML", "CSS"],
+    authors: [{ name: "denis-frontend", url: siteUrl }],
+    creator: "denis-frontend",
+    metadataBase: new URL(siteUrl),
+    openGraph: {
+        title: "Frontend Web Development",
+        description: "Discover projects and insights from a frontend developer passionate about building modern, fast, and accessible web apps.",
+        url: siteUrl,
+        siteName: "Frontend Web Development",
+        images: [
+            {
+                url: "/og-image.jpg",
+                width: 1200,
+                height: 630,
+                alt: "Frontend Developer Portfolio Preview",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Frontend Web Development",
+        description: "Discover projects and insights from a frontend developer passionate about building modern, fast, and accessible web apps.",
+        images: ["/og-image.jpg"],
+    },
 };
+
 
 export default function RootLayout({
                                        children,
